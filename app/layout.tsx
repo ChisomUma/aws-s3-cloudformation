bc3764c69c43d2406c9f10112a28e08878759811
chisomuma <chisom.uma@gbgplc.com>
2025-07-13 21:15:22 +0100

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col">
-        <Header/>
+        <Header />
         <main className="flex-grow">
           {children}
         </main>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   )
